f6b7d502ac82e21e15fc6758676d0cd1901177e3
Chandra Malti Farm <maltifarm@gmail.com>
2026-01-03 15:33:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7674284923-3e6d9",
+  "appId": "1:5985593020:web:b9eba7d777ea656afc52b9",
+  "apiKey": "AIzaSyAXcAxwRVWudbpFZiAoToQ7zBG4aITXb2c",
+  "authDomain": "studio-7674284923-3e6d9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "5985593020"
+};
